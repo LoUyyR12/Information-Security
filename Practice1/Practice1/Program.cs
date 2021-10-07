11d21fcb14ce7a6faf8d0936643ec85f2035e91c
Volodymyr <LoUyyR12@gmail.com>
2021-10-07 17:56:00 +0300

    Practice 1 done

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace Practice1
 {
     class Program
     {
+        static byte[] GetCryptoRng(int length = 10)
+        {
+            var rngGen = new RNGCryptoServiceProvider();
+            var rndNumber = new byte[length];
+            rngGen.GetBytes(rndNumber);
+            return rndNumber;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random(3247);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(-100, 100));
+            }
+            Console.WriteLine("-------------------------");
+            Random random1 = new Random(3247);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random1.Next(-100, 100));
+            }
+            Console.WriteLine("-------------------------");
+            for (int i = 0; i < 4; i++)
+            {
+                string textRNGs = Convert.ToBase64String(GetCryptoRng());
+                Console.WriteLine(textRNGs);
+            }
         }
+        
     }
 }
